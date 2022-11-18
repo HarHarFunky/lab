3c0ac9339fe4d6e272e368979352a86008ab2da2
HarHarFunky <60793820+HarHarFunky@users.noreply.github.com>
2022-11-18 17:02:27 +0000

    Create account.py

---
+++ account.py
@@ -0,0 +1,19 @@
+class Account():
+    def __init__(self,name):
+        _account_name = name
+        _account_balance = 0 # Cause you're broke
+    def deposit(self, ammount):
+        if ammount > 0:
+            self._account_balance+=ammount
+            
+            
+            return True
+        return False
+    def withdraw(self, ammount):
+        if ammount > 0:
+            if ammount <= self._account_balance:
+                self._account_balance-=ammount
+                return True
+        return False
+    def get_balance(self): return self._account_balance
+    def get_name(self): return self._account_name
