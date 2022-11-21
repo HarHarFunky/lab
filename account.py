406a6399e7f6d458996cfe96cd53a917735cda78
HarHarFunky <60793820+HarHarFunky@users.noreply.github.com>
2022-11-21 01:01:38 +0000

    Update account.py

--- account.py
+++ account.py
@@ -1,19 +1,19 @@
 class Account():
-    def __init__(self,name):
-        self._account_name = name
-        self._account_balance = 0 # Cause you're broke
+    def __init__(self,name: str):
+        self.__account_name = name
+        self.__account_balance = 0 # Cause you're broke
     def deposit(self, ammount):
         if ammount > 0:
-            self._account_balance+=ammount
+            self.__account_balance+=ammount
             
             
             return True
         return False
     def withdraw(self, ammount):
         if ammount > 0:
-            if ammount <= self._account_balance:
-                self._account_balance-=ammount
+            if ammount <= self.__account_balance:
+                self.__account_balance-=ammount
                 return True
         return False
-    def get_balance(self): return self._account_balance
-    def get_name(self): return self._account_name
+    def get_balance(self): return self.__account_balance
+    def get_name(self): return self.__account_name
