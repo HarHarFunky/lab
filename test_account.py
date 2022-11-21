9dd0fd24d75e78d38711aad2d5b7c11126598a43
HarHarFunky <60793820+HarHarFunky@users.noreply.github.com>
2022-11-21 01:23:48 +0000

    Update test_account.py
    extraneous tests added

--- test_account.py
+++ test_account.py
@@ -1,4 +1,5 @@
 from account import Account
+from pytest import *
 
 def test_init():
     account_one = Account('john')
@@ -8,11 +9,15 @@ def test_init():
 def test_deposit():
     account_one = Account('john')
     assert account_one.deposit(1000000) is True
-    assert account_one.get_balance() == 1000000
+    assert account_one.deposit(-1000000) is False
+    assert account_one.deposit(0) is False
+    assert account_one.get_balance() == approx(1000000,abs=0.001)
 
 def test_withdraw():
     account_one = Account('john')
     assert account_one.deposit(1000000) is True
     assert account_one.withdraw(500000) is True
     assert account_one.withdraw(750000) is False
+    assert account_one.withdraw(-750000) is False
+    assert account_one.withdraw(0) is False
     assert account_one.get_balance() == 500000
