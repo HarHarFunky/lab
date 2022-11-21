767124ae9771bed2b328be03b87e1e95aa9c7060
HarHarFunky <60793820+HarHarFunky@users.noreply.github.com>
2022-11-21 01:30:47 +0000

    Update account.py
    Removed test branch only features

--- account.py
+++ account.py
@@ -1,39 +1,21 @@
 class Account():
-    def __init__(self,name: str):
-        '''
-        This is the initializer for the entire class
-        :param name: This is what you will call the account
-        '''
+    def __init__(self,name):
         self.__account_name = name
         self.__account_balance = 0 # Cause you're broke
-    def deposit(self, ammount: float):
-        '''
-        This is a function to add money to your account
-        :param ammount: How much money do you want to add?
-        '''
+    def deposit(self, ammount):
         if ammount > 0:
             self.__account_balance+=ammount
             
             
             return True
         return False
-    def withdraw(self, ammount: float):
-        '''
-        This is a function to take money from your account
-        :param ammount: How much money do you want to take?
-        '''
+    def withdraw(self, ammount):
         if ammount > 0:
             if ammount <= self.__account_balance:
                 self.__account_balance-=ammount
                 return True
         return False
     def get_balance(self):
-        '''
-        Get your account balance
-        '''
         return self.__account_balance
     def get_name(self):
-        '''
-        Get the account name
-        '''
         return self.__account_name
